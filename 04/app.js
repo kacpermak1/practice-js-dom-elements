e77e240468d3b3e2852165af99b608ab22301c6d
Kacper Makówka <kacpermak24@gmail.com>
2025-02-18 00:25:07 +0100

    Task 04

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,19 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const navList = document.createElement('ul');
+
+menuItems.forEach(item => {
+    const listItem = document.createElement('li');
+    const listItemLink = document.createElement('a');
+
+    listItemLink.href = item.url;
+    listItemLink.textContent = item.text;
+
+    listItem.appendChild(listItemLink);
+    navList.appendChild(listItem);
+});
+
+const nav = document.querySelector('nav');
+nav.appendChild(navList);
