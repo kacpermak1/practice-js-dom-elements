dbd3a0269f7b01b9eb899f910e9bf6b3da1767c3
Kacper Makówka <kacpermak24@gmail.com>
2025-02-17 22:56:39 +0100

    Task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const commentsNewest = document.querySelector('.comments__item.comments__item--newest');
+
+if (commentsNewest) {
+  const commentsNewestWithDataInfo = commentsNewest.querySelectorAll('[data-info]');
+  console.log(`${commentsNewestWithDataInfo.length} elements found with data-info attribute.`);
+};
