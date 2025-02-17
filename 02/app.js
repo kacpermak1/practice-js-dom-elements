d315e82720818442419f527475ff206227b3dc39
Kacper Makówka <kacpermak24@gmail.com>
2025-02-17 23:25:21 +0100

    Task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const linksWithDataUrl = document.querySelectorAll('a[data-url]');
+
+linksWithDataUrl.forEach(link => {
+    const url = link.getAttribute('data-url');
+    link.setAttribute('href', url);
+});
