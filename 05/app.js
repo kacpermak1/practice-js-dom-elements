11c78ca98c5259247abcf4b4794b2d31697735ca
Kacper Makówka <kacpermak24@gmail.com>
2025-02-18 01:28:56 +0100

    Task 05

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,44 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+if (!curr) {
+    console.error('.js-curr elem not found');
+} else {
+    const currParent = curr.parentElement;
+    const articlesSection = currParent.parentElement;
+    const articles = articlesSection.children;
+    const lastArticle = articles[articles.length - 1];
+    const newArticle = lastArticle.cloneNode(true); // Used the last article as a template
+
+    //1
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Usuń z koszyka';
+    currParent.insertBefore(removeButton, curr.nextElementSibling);
+
+    //2
+    for (let child of currParent.children) {
+        if (child !== curr) child.classList.add('siblings');
+    }
+
+    //3
+    const nextArticle = currParent.nextElementSibling;
+    if (nextArticle && nextArticle.classList.contains('article')) nextArticle.setAttribute('title', 'nextElementSibling');
+
+    //4
+    if (lastArticle) {
+        const extraParagraph = document.createElement('p');
+        extraParagraph.textContent = 'Extra paragraph';
+        const btnInLast = lastArticle.querySelector('button');
+
+        btnInLast ? lastArticle.insertBefore(extraParagraph, btnInLast) : lastArticle.appendChild(extraParagraph);
+    }
+
+    //5
+    const titleEl = newArticle.querySelector('.article__title');
+    if (titleEl) titleEl.textContent = 'JS - New Article';
+
+    const descEl = newArticle.querySelector('.article__description');
+    if (descEl) descEl.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+
+    articlesSection.insertBefore(newArticle, articlesSection.firstElementChild);
+}
